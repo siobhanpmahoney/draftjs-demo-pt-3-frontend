cc9c9d0438933a532face58e33a25a12c8196f5d
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-06-24 05:00:28 -0400

    able to save content to server, still need to render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import React, { Component } from "react";
 import "./App.css";
+import NoteListContainer from "./components/noteList/NoteListContainer"
 import PageContainer from "./components/PageContainer";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as Actions from '../src/actions'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			displayedNote: null,
+		}
+	}
+
+	componentDidMount() {
+		this.setState({
+			displayedNote: "new"
+		})
+	}
+
+	selectNote = (event) => {
+		console.log("in selectNote")
+		let target_id = event.target.id
+		let selected =  this.props.notes.find((note) => {
+			return note.id == target_id
+		})
+		console.log(selected)
+		this.setState({
+			displayedNote: selected
+		})
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<PageContainer />
+				<NoteListContainer selectNote={this.selectNote}/>
+				<PageContainer displayedNote={this.state.displayedNote} />
 			</div>
 		);
 	}
 }
 
-export default App;
+function mapStateToProps(state, props) {
+  return {
+    notes: state.notes.allNotes,
+		displayedNote: state.notes.displayedNote
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,32 +1,69 @@
 import React from "react";
-import { EditorState, RichUtils } from "draft-js";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as Actions from '../actions'
+import { EditorState, RichUtils, convertToRaw, convertFromRaw } from "draft-js";
 import Editor, { composeDecorators } from "draft-js-plugins-editor";
 import createHighlightPlugin from "./plugins/highlightPlugin";
 import addLinkPlugin from "./plugins/addLinkPlugin";
 
-import "draft-js-image-plugin/lib/plugin.css";
 const highlightPlugin = createHighlightPlugin();
-
 
 
 class PageContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			noteTitle: "",
 			editorState: EditorState.createEmpty()
 		};
 
-		this.plugins = [ 
+		this.plugins = [
 			addLinkPlugin,
 			highlightPlugin,
 		];
+	}
+
+
+	componentDidMount() {
+		let displayedNote = this.props.displayedNote
+		if (displayedNote == "new" ) {
+			editorState: EditorState.createEmpty()
+		} else if (!!displayedNote && !!displayedNote.id) {
+			editorState: EditorState.createWithContent(convertFromRaw(this.props.displayedNote.content))
+		}
+
 	}
 
 	onChange = editorState => {
 		this.setState({
 			editorState
 		});
-	};
+	}
+
+	submitEditor = () => {
+		debugger
+		let displayedNote = this.props.displayedNote
+		let contentState = this.state.editorState.getCurrentContent()
+		if (displayedNote == "new") {
+			let noteTitle = this.state.noteTitle
+			let note = {title: noteTitle, content: convertToRaw(contentState)}
+			note["content"] = JSON.stringify(note.content)
+			console.log(note)
+			this.props.createNote(note.title, note.content)
+		} else if (!!displayedNote && !!displayedNote.id) {
+			this.props.updateNote(displayedNote.id, this.state.noteTitle, convertToRaw(this.state.editorState))
+
+		}
+	}
+
+	captureTitle = (event) => {
+		event.preventDefault()
+		let value = event.target.value
+		this.setState({
+			noteTitle: value
+		})
+	}
 
 	handleKeyCommand = command => {
 		const newState = RichUtils.handleKeyCommand(
@@ -59,8 +96,9 @@ class PageContainer extends React.Component {
 	render() {
 		return (
 			<div className="editorContainer">
+
 				<button onClick={this.onUnderlineClick}>
-				U
+					U
 				</button>
 				<button onClick={this.onBoldClick}>
 					<b>B</b>
@@ -68,17 +106,31 @@ class PageContainer extends React.Component {
 				<button onClick={this.onItalicClick}>
 					<em>I</em>
 				</button>
+				<div className="aboveEditor">
+					<div><button className="submitNote" onClick={this.submitEditor}>Save</button></div>
+					<input type="text" name="noteTitle" className="noteTitle" value={this.state.noteTitle} onChange={this.captureTitle}/>
+				</div>
 				<div className="editors">
 					<Editor
 						editorState={this.state.editorState}
 						handleKeyCommand={this.handleKeyCommand}
 						plugins={this.plugins}
 						onChange={this.onChange}
-					/>
+						/>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default PageContainer;
+function mapStateToProps(state, props) {
+	return {
+		notes: state.notes.allNotes,
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageContainer)
